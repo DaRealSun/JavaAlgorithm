91cab6256545c5a05ca86f88c457acdced961509
DaRealSun <info.leminhson@gmail.com>
2025-07-02 16:22:04 -0400

    Max method 3 vars

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import static java.lang.Math.max;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,15 +11,24 @@ public class Main {
         input.add(0);
         input.add(2);
         input.add(3);
+        int a = 1;
+        int b = 0;
+        int c = -1;
         maxNumber(input);
+        maxOfThree(a,b,c);
+    }
 
+
+    private static void maxOfThree(int a, int b, int c) {
+        int maxNum = max(max(a, b), c);
+        System.out.println(maxNum);
     }
 
     static void maxNumber(ArrayList<Integer> input) {
         int maxNum= input.getFirst();
         for (Integer integer : input) {
 
-            maxNum = Math.max(maxNum, integer);
+            maxNum = max(maxNum, integer);
 
         }
         System.out.println(maxNum);
